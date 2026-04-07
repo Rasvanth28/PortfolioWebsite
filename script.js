038ebfe2dd0011a711b49bec7e1eedfd5124a5ef
Rasvanth28 <rasvanthkc2005@gmail.com>
2026-04-07 17:26:31 +0530

    Added icons to the technical stack and made used a offline video

--- script.js
+++ script.js
@@ -325,7 +325,7 @@ function initConsole() {
 
     function triggerRickroll() {
         overlay.classList.remove('hidden');
-        playerDiv.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        playerDiv.innerHTML = `<video src="reveal.mp4" autoplay controls style="width: 100%; height: 100%;"></video>`;
     }
 }
 
